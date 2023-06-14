8ae83d749fb0a75731083e601bbdba1c1bc05d5f
Patrick Pulwey <patrick.pulwey@cloudbucket.de>
2023-06-14 17:37:56 +0200

    Deploy und test app

--- testapp/index.js
+++
@@ -1,8 +0,0 @@
-// Basic express server
-const express = require("express");
-const app = express();
-const port = process.env.PORT;
-
-app.get("/", (req, res) => res.send("Hello World!"));
-
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
---
+++ testapp/src/index.ts
@@ -0,0 +1,13 @@
+// Basic express server
+import express from "express";
+
+const app = express();
+const port = process.env.PORT;
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
